d781dba499a64df8897fcaa4e1b6843a878e14c0
SEEengineering <57258143+SEEengineering@users.noreply.github.com>
2020-01-19 19:32:52 +0000

    Add files via upload

---
+++ UnrealBP/Source/VrProjectCollege/VRCharacter.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+	DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DestinationMarker"));
+	DestinationMarker->SetupAttachment(GetRootComponent());
+
+	Camera->GetForwardVector();
+
+	
+
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewCameraOffset = - Camera->GetComponentLocation() - GetActorLocation();
+	NewCameraOffset.Z = 0;
+	AddActorWorldOffset(NewCameraOffset);
+	VRRoot->AddWorldOffset(-NewCameraOffset);
+
+}
+void AVRCharacter::UpdateDestinationMarker()
+{
+	FVector Start= Camera->GetComponentLocation();
+	FVector End = Start + Camera->GetForwardVector()* MaxTeleportDistance;
+
+	FHitResult HitResult;
+	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
+
+	if (bHit)
+	{
+		DestinationMarker->SetWorldLocation(HitResult.Location);
+	}
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveRight);
+
+}
+
+
+
+void AVRCharacter::MoveForward(float throttle) {
+
+	AddMovementInput(throttle * Camera->GetForwardVector());
+
+}
+void AVRCharacter::MoveRight(float throttle) {
+
+	AddMovementInput(throttle * Camera->GetRightVector());
+
+}
+
